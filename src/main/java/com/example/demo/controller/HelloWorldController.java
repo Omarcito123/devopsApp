9a6c7a5735894c75c5c3427f6408c12b9af7870a
Marvin <m.omar.2007@hotmail.com>
2024-02-24 21:01:22 -0600

    upload service

---
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/home")
+    public String HelloWorld(){
+        return "Hello World";
+    }
+}
